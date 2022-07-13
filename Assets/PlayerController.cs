09abbfd085dd8d503dcca4d03e0d96fe96c5a782
Yannis Lagrosa <378940@ga.aliefisd.net>
2022-07-13 15:41:24 -0500

    Updated Files.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,10 +6,25 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public Rigidbody2D rigidbody;
     Vector2 movement;
+    public Vector2 mousePos;
+    public GameObject staff;
     void Update()
     {
+        mousePos = Input.mousePosition;
+        mousePos.Normalize();
+        // A-B = Vector from A To B
+        Vector3 diff = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        // Normalize else Quaternion if pure
+        diff = diff.normalized;
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-       rigidbody.MovePosition(new Vector2(transform.position.x,transform.position.y)+movement.normalized * Time.fixedDeltaTime * speed);
+        rigidbody.MovePosition(new Vector2(transform.position.x, transform.position.y) + movement.normalized * Time.fixedDeltaTime * speed);
+
+        //Let's use quaternions for the staff rotation.
+
+        //Add the Angle (Atan2 gets Vector to angle in radians) convert to degrees.
+        // Either of these can work. : )
+        staff.transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg - 90, Vector3.forward);
+        //staff.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg - 90);
     }
 }
